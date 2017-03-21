97ab41d41a3cc37c984f129a5242daefaa6a469b
Daril Alanis <a.alanis1108@gmail.com>
2017-03-21 21:23:21 +0000

    my comment

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
+    var ScriptEders = ["Lauren","Pamela","Lauren","Felix","Isabela","Gabrielle","Anahi"];
+    var ScriptEducators = ["Jenna","Nick","Matthew","Michael"];
+    var random2 = Math.floor(Math.random()*ScriptEducators.length);
+
+    //console.log(ScriptEders[random]);
+    console.log(ScriptEducators[random2]);
 
 
+    $("#studentButton").click(function(){
+            var random = Math.floor(Math.random()*ScriptEders.length);
 
+    $("#studentDisplay").html(ScriptEders[random]);
+    });
 });
